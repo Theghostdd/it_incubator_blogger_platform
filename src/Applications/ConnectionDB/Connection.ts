2939e4db8ef7eb76d8678ffb71a596824b5890e9
Thedd <mixailmar4uk78@gmail.com>
2024-07-22 14:46:41 +0300

    change url for mongo atlas

--- src/Applications/ConnectionDB/Connection.ts
+++ src/Applications/ConnectionDB/Connection.ts
@@ -5,7 +5,7 @@ import * as mongoose from "mongoose";
 
 export async function startDB() {
     try {
-        await mongoose.connect(MONGO_SETTINGS.URL, {dbName: MONGO_SETTINGS.DB_NAME})
+        await mongoose.connect(MONGO_SETTINGS.URL_CLOUD, {dbName: MONGO_SETTINGS.DB_NAME})
         console.log("Connect to DB")
     } catch (e: any) {
         await SaveError('/connect-to-db', 'CONNECT DB', 'Connecting to the database', e)
